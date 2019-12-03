aaff7c36a88675c646ee14ad7813c06c9c9a4f1c
Estelle Choi <estele.choi@gmail.com>
2019-12-04 04:03:44 +0900

    뭔지 모를 설정파일들

---
+++ firstweb/src/com/youjin/ReadHeaderServlet.java
@@ -0,0 +1,45 @@
+package com.youjin;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/ReadHeaderServlet")
+public class ReadHeaderServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public ReadHeaderServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter writer = response.getWriter();
+		writer.println("<html>");
+		writer.println("<head><title>hello world</title></head>");
+		writer.println("</body>");
+		
+		Enumeration<String> headerNames = request.getHeaderNames();
+		while(headerNames.hasMoreElements()) {
+			String headerName = headerNames.nextElement();
+			String headerValue = request.getHeader(headerName);
+			writer.println(headerName + " : " + headerValue + "<br />");
+		}
+		
+		writer.println("</body>");
+		writer.println("</html>");
+		writer.close();
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
